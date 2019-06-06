87b19e273c0f1fec8f83c4c9a732aa0a6e985cac
oscarpw <oscar.castillo@primeraweb.cl>
2019-06-05 23:48:29 -0400

    desafio filtro hash 2.0

--- desafio_filtrando_hash.rb
+++ desafio_filtrando_hash.rb
@@ -1,15 +1,17 @@
-ventas = {
+def filter (hash)
+
+    filtro = {}
+
+    hash.each do |k,v|
+        if v < 70000
+            filtro[k] = v
+        end
+    end
+    puts filtro
+end
+
+filter ({
     Octubre:6500,
     Noviembre:68000,
     Diciembre:72000
-}
-
-filtro = {}
-
-ventas.each do |k,v|
-    if v < 70000
-        filtro[k] = v
-    end
-end
-
-puts filtro
+})
